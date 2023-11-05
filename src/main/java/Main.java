450c7abe63953771396559325c2e8b094a9799f1
oceane <oceane.copub@gmail.com>
2023-11-05 11:27:24 +0100

    fin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,11 +16,9 @@ public class Main {
         Effet1 e1= new Effet1();
         Effet2 e2= new Effet2();
         Effet3 e3= new Effet3();
-        Effet4 e4= new Effet4();
         lesEffets.add(e1);
         lesEffets.add(e2);
         lesEffets.add(e3);
-        lesEffets.add(e4);
 
 
         Scenario scena = new Scenario(lesEffets, monBandeau);
